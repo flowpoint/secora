05cef9786d70de66060f8bbf6414aff43f669992
flowpoint <flowpoint@protonmail.com>
2021-10-08 20:19:39 +0200

    start with a modified example of simcse from sbert

---
+++ mrr.py
@@ -0,0 +1,25 @@
+from itertools import starmap
+import numpy as np
+
+# my testimplementation of mrr
+def rr(relevant_id: str, ranking: [str]):
+    rrank = 1 / (ranking.index(relevant_id)+1)
+    return rrank
+
+rr(1, [1,2,3])
+
+def mrr(true_ids: [str], rankings: [[str]]):
+    return np.mean(list(starmap(rr, zip(true_ids, rankings))))
+
+
+# example of wikipedia
+trues = ['cats', 'tori', 'viruses']
+
+rankings = [
+    ['catten', 'cati', 'cats'],
+    ['torii', 'tori', 'toruses'],
+    ['viruses', 'virii', 'viri'],
+    ]
+
+mrr(trues, rankings)
+
